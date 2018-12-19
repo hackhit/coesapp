d64a24c39786cd4a34a09162384e941213dc31e4
Daniel Moros <aceim.development@gmail.com>
2018-12-19 00:55:21 -0400

    Primeros ajustes de configuración y barras de navegación

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,3 +14,24 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
+
+ActiveSupport::Inflector.inflections(:es) do |inflect|
+  # inflect.plural /^(ox)$/i, '\1en'
+  # inflect.singular /^(ox)en/i, '\1'
+  # inflect.irregular 'person', 'people'
+  # inflect.uncountable %w( fish sheep )
+
+  inflect.plural /([^djlnrs])([A-Z]|_|$)/, '\1s\2'
+  inflect.plural /([djlnrs])([A-Z]|_|$)/, '\1es\2'
+  inflect.plural /(.*)z([A-Z]|_|$)$/i, '\1ces\2'
+  
+  inflect.singular /([^djlnrs])s([A-Z]|_|$)/, '\1\2'
+  inflect.singular /([djlnrs])es([A-Z]|_|$)/, '\1\2'
+  inflect.singular /(.*)ces([A-Z]|_|$)$/i, '\1z\2'
+
+  inflect.irregular 'user', 'users'
+  inflect.irregular 'account', 'accounts'
+  inflect.irregular 'password', 'passwords'
+  inflect.irregular 'session', 'sessions'
+
+end
