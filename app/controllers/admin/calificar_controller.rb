ecbad7c32ad92f46505158213638502a73fe80fa
Daniel Moros <aceim.development@gmail.com>
2019-01-04 03:07:13 -0400

    Ajustes en controlador Calificar

--- app/controllers/admin/calificar_controller.rb
+++ app/controllers/admin/calificar_controller.rb
@@ -1,32 +1,25 @@
 module Admin
 	class CalificarController < ApplicationController
 
-		before_filter :cal_filtro_logueado
-		before_filter :cal_filtro_admin_profe
+		before_action :filtro_logueado
+		before_action :filtro_admin_profe
 		# OJO: POR REVISAR
 		def seleccionar_seccion
-			@profesor = CalProfesor.find (session[:cal_profesor].cal_usuario_ci)
-			@periodo_actual = CalParametroGeneral.cal_semestre_actual
+			@profesor = Profesor.find (session[:profesor_id])
+			@periodo_actual = ParametroGeneral.periodo_actual_id
 			@titulo = "Secciones disponibles para este período #{@periodo_actual.id}"
-			@cal_secciones = @profesor.cal_secciones.where(:cal_semestre_id => @periodo_actual.id)
-			@cal_secciones_secundarias = @profesor.cal_secciones_secundarias.where(:cal_semestre_id => @periodo_actual.id)
+			@secciones = @profesor.secciones.where(:semestre_id => @periodo_actual.id)
+			@secciones_secundarias = @profesor.secciones_secundarias.where(:periodo_id => @periodo_actual.id)
 		end
 
 		def ver_seccion
-			id = params[:id]
-			@cal_seccion = CalSeccion.find(id)
+			id = 
+			@seccion = Seccion.find params[:id]
 
-			@estudiantes_secciones = @cal_seccion.cal_estudiantes_secciones.sort_by{|h| h.cal_estudiante.cal_usuario.apellidos}
+			@estudiantes_secciones = @seccion.inscripcionsecciones.sort_by{|h| h.estudiante.usuario.apellidos}
 
-
-			#if @cal_seccion.cal_semestre_id.eql? '2017-02A'
-			#	@estudiantes_secciones = @cal_seccion.cal_estudiantes_secciones.confirmados.sort_by{|h| h.cal_estudiante.cal_usuario.apellidos}
-			#else
-			#	@estudiantes_secciones = @cal_seccion.cal_estudiantes_secciones.sort_by{|h| h.cal_estudiante.cal_usuario.apellidos}
-			#end
-
-			@titulo = "Sección: #{@cal_seccion.descripcion} - Período #{@cal_seccion.cal_semestre_id}"
-			if @cal_seccion.cal_materia.cal_categoria_id.eql? 'IB' or @cal_seccion.cal_materia.cal_categoria_id.eql? 'LIN' or @cal_seccion.cal_materia.cal_categoria_id.eql? 'LE'
+			@titulo = "Sección: #{@seccion.descripcion} - Período #{@seccion.periodo_id}"
+			if @seccion.asignatura.catedra_id.eql? 'IB' or @seccion.asignatura.catedra_id.eql? 'LIN' or @seccion.asignatura.catedra_id.eql? 'LE'
 				@p1 = 25 
 				@p2 =35
 				@p3 = 40
@@ -42,46 +35,46 @@ module Admin
 
 			id = params[:id]
 
-			@cal_seccion = CalSeccion.find(id.split(" "))
+			@seccion = Seccion.find(id.split(" "))
 
 			@estudiantes = params[:est]
 
 			@estudiantes.each_pair do |ci,valores|
 
-				@cal_estudiante_seccion = @cal_seccion.cal_estudiantes_secciones.where(:cal_estudiante_ci => ci).limit(0).first
+				@inscripcionsecciones = @seccion.inscripcionsecciones.where(estudiante_id: id).limit(0).first
 				
 				if valores['pi']
-					cal_tipo_estado_calificacion_id = 'PI'
+					tipo_estado_calificacion_id = 'PI'
 				else
 					if valores[:calificacion_final].to_f >= 10
-						cal_tipo_estado_calificacion_id = 'AP'
+						tipo_estado_calificacion_id = 'AP'
 					else 
-						cal_tipo_estado_calificacion_id = 'RE'
+						tipo_estado_calificacion_id = 'RE'
 					end
 				end
-				valores['cal_tipo_estado_calificacion_id'] = cal_tipo_estado_calificacion_id
-				unless @cal_estudiante_seccion.update_attributes(valores)
+				valores['tipo_estado_calificacion_id'] = tipo_estado_calificacion_id
+				unless @estudiante_seccion.update_attributes(valores)
 					flash[:danger] = "No se pudo guardar la calificación."
 					break
 				end
 
 			end
-			@cal_seccion.calificada = true
-			calificada = @cal_seccion.save
+			@seccion.calificada = true
+			calificada = @seccion.save
 
 			flash[:success] = "Calificaciones guardada satisfactoriamente." if calificada
 
-			if session[:cal_administrador]
-				redirect_to :controller => 'cal_principal_admin'
+			if session[:administrador]
+				redirect_to :controller => 'principal_admin'
 			else
-				redirect_to :action => "ver_seccion", :id => @cal_estudiante_seccion.cal_seccion.id
+				redirect_to :action => "ver_seccion", :id => @estudiante_seccion.seccion.id
 			end
 		end
 
 		def descargar_notas
 			id = params[:id]
 			seccion = Seccion.find(id)
-			pdf = DocumentosPDF.cal_notas seccion
+			pdf = DocumentosPDF.notas seccion
 			unless send_data pdf.render,:filename => "notas_#{seccion.asignatura_id}_#{seccion.numero}.pdf",:type => "application/pdf", :disposition => "attachment"
 		    	flash[:mensaje] = "en estos momentos no se pueden descargar las notas, intentelo luego."
 		    end
