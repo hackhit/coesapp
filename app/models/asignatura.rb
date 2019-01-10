5190db958794dd03b78761645036614531f33e98
Daniel Moros <aceim.development@gmail.com>
2019-01-10 18:07:29 -0400

    Agregada calificacion al modelo de asignatura y cambio en descripcion completa

--- app/models/asignatura.rb
+++ app/models/asignatura.rb
@@ -6,6 +6,9 @@ class Asignatura < ApplicationRecord
 	belongs_to :tipoasignatura
 	# belongs_to :catedra_departamento, class_name: 'CatedraDepartamento', foreign_key: [:catedra_id, :departamento_id], primary_key: [:catedra_id, :departamento_id]
 
+	# ENUMERADAS CONSTANTES
+	enum calificacion: [:numerica, :absoluta]
+
 	has_many :secciones
 	accepts_nested_attributes_for :secciones
 
@@ -14,6 +17,7 @@ class Asignatura < ApplicationRecord
 	validates_uniqueness_of :id_uxxi, message: 'Código UXXI ya está en uso', field_name: false	
 	validates_presence_of :id_uxxi, message: 'Código UXXI requerido'	
 	validates :descripcion, presence: true
+	validates :calificacion, presence: true
 	# validates :anno, presence: true
 	# validates :orden, presence: true
 	validates :catedra_id, presence: true
@@ -26,6 +30,7 @@ class Asignatura < ApplicationRecord
 
 	# TRIGGGERS:
 	before_save :set_uxxi_how_id
+	before_save :set_to_upcase
 
 	# FUNCIONES:
 
@@ -33,22 +38,23 @@ class Asignatura < ApplicationRecord
 		return self.activa #self.activa.eql? true ? true : false
 	end
 	def descripcion_completa
-		desc = "#{descripcion} "
-		desc += "- #{catedra.descripcion}" if catedra
-		desc += "- #{departamento.descripcion}" if departamento
-		return desc
+		"#{descripcion.titleize} #{catedra.descripcion_completa}"
 	end
 
 	def descripcion_reversa
 		desc = cal_departamento.descripcion if cal_departamento
-		desc += "| #{catedra.descripcion}" if catedra		
+		desc += "| #{catedra.descripcion.titleize}" if catedra		
 		return desc
 	end
 
 	# FUNCIONES PROTEGIDAS:
 	protected
+	
 	def set_uxxi_how_id
 		self.id ||= self.id_uxxi
 	end
 
+	def set_to_upcase
+		self.descripcion = self.descripcion.upcase
+	end
 end
