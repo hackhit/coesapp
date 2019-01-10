22cb3a147995686f02be26f57b0d6e9fac854eec
Daniel Moros <aceim.development@gmail.com>
2019-01-10 18:06:44 -0400

    Agregado rol admin_departamento a Administrador

--- app/models/administrador.rb
+++ app/models/administrador.rb
@@ -1,46 +1,69 @@
 class Administrador < ApplicationRecord
 
 	# VARIABLES
-	enum rol: [:super, :jefe_departamento, :taquilla]
+	enum rol: [:super, :admin_escuela, :admin_departamento, :taquilla]
 
 	# TRIGGERS
 	after_initialize :set_default_taquilla, if: :new_record?
 
-	before_save :set_dpto_to_nil
+	before_save :set_to_nil
 
 	# ASOCIACIONES
 	belongs_to :usuario, foreign_key: :usuario_id 
-	accepts_nested_attributes_for :usuario
 
 	belongs_to :departamento, optional: true
-	accepts_nested_attributes_for :departamento
+
+	belongs_to :escuela, optional: true
 
 	# VALIDACIONES
 	validates :usuario_id,  presence: true, uniqueness: true
-	validates :departamento_id,  presence: true, if: -> {self.jefe_departamento?}
+	validates :departamento_id,  presence: true, if: -> {self.admin_departamento?}
+	validates :escuela_id,  presence: true, if: -> {self.admin_escuela?}
 
 	# FUNCIONES
+	def escuelas
+		if self.super?
+			return Escuela.all
+		elsif self.admin_escuela? 
+			return Escuela.where(id: self.escuela_id)
+		else
+			return Escuela.where(id: self.departamento.escuela_id)
+		end
+		
+	end
 
-	# FUNCIONES PROTEGIDAS
-	def set_dpto_to_nil
-		self.departamento_id = nil if self.departamento_id.blank?
+	def departamentos
+		if self.escuela_id
+			self.escuela.departamentos
+		elsif self.departamento_id
+			Departamento.where(id: departamento_id)
+		else
+			Departamento.all
+		end
 	end
 
 	def altos?
-		self.super? or self.jefe_departamento?
+		self.super? or self.admin_departamento?
 	end
 
 	def descripcion
-		"#{usuario.descripcion} - #{rol.titleize}"
+		aux = "#{usuario.descripcion} - #{rol.titleize}"
+		aux += " (#{self.departamento.descripcion})" if self.departamento
+		aux += " (#{self.escuela.descripcion})" if self.escuela
+		return aux
 	end
 	
+	# FUNCIONES PROTEGIDAS
 	protected
-
-
 	def set_default_taquilla
 		self.rol ||= :taquilla
 	end
+	def set_to_nil
+		self.departamento_id = nil if self.departamento_id.blank?
+		self.escuela_id = nil if self.escuela_id.blank?
+	end
 
-
-
+	def puede_escribir?
+		!self.taquilla?
+	end
 end
