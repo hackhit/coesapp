9238a0a2d2459265869e9e3d3d9b640b0cb5e137
Daniel Moros <aceim.development@gmail.com>
2019-02-07 14:35:40 -0400

    Mejora de vista de profesores
    Se incorpora un parcial (card_seccion) para las secciones

--- app/controllers/admin/principal_profesor_controller.rb
+++ app/controllers/admin/principal_profesor_controller.rb
@@ -8,9 +8,9 @@ module Admin
 			@usuario = Usuario.find session[:profesor_id]
 			@titulo = "Principal - Profesores"	
 			@profesor = Profesor.find (session[:profesor_id])
-			@secciones_pendientes = @profesor.secciones.sin_calificar#.del_periodo current_periodo.id
-			@secciones_calificadas = @profesor.secciones.calificadas
-			@secciones_secundarias = @profesor.secciones_secundarias#.where(periodo_id: @periodo_actual_id)
+			@secciones_pendientes = @profesor.secciones.sin_calificar.order('periodo_id DESC, numero ASC')
+			@secciones_calificadas = @profesor.secciones.calificadas.order('periodo_id DESC, numero ASC')
+			@secciones_secundarias = @profesor.secciones_secundarias.order('periodo_id DESC, numero ASC')#.where(periodo_id: @periodo_actual_id)
 		end
 
 	end
