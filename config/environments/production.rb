021fe28c5751a95fd0d8e7baa134d31c24c14b43
Daniel Moros <aceim.development@gmail.com>
2019-04-23 14:48:26 -0400

    Ajustes en env_prod para email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,12 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: Rails.application.secrets.address,
-    port: 587,
+    port: 25,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password,
-    openssl_verify_mode: 'none' 
+    password: Rails.application.secrets.email_provider_password 
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
