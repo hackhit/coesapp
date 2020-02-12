bb85c504d78f42559f43cf9d524a744d6fa7079c
Daniel Moros <aceim.development@gmail.com>
2020-02-12 17:18:03 -0400

    Avances en la incorporación de sistema de autorización

--- app/controllers/admin/autorizadas_controller.rb
+++ app/controllers/admin/autorizadas_controller.rb
@@ -8,26 +8,45 @@ module Admin
 		end
 
 		def set
-			if autorizada = Autorizada.where(restringida_id: params[:id], usuario_id: params[:usuario_id]).first
-
-				respond_to do |format|
-					if autorizada.destroy
-						format.json {render json: 'Desautorizado', status: :ok }
-					else
-						format.json {render json: 'Error al intentar desauthorizar', status: :unprocessable_entity }
-					end
-				end
+			if params[:grupo]
+				params[:id] = Restringida.where(grupo: params[:id]).ids
 			else
-				autorizada = Autorizada.new(restringida_id: params[:id], usuario_id: params[:usuario_id])
-
-				respond_to do |format|
-					if autorizada.save
-						format.json {render json: 'Authorizado', status: :ok }
-					else
-						format.json {render json: 'Error al intentar autorizar', status: :unprocessable_entity }
-					end
-				end
+				params[:id] = [params[:id]]
 			end
+
+			if params[:autorizar].eql? 'true'
+				
+				if usuario = Usuario.find(params[:usuario_id])
+					u_ids = usuario.restringidas.ids
+					params[:id].reject!{|id| u_ids.include? id}
+				end
+				Autorizada.create(params[:id].map{|id| {restringida_id: id, usuario_id: params[:usuario_id]}}) if params[:id].any?
+			else
+				Autorizada.where(restringida_id: params[:id], usuario_id: params[:usuario_id]).delete_all
+			end
+
+			render json: 'Desautorizado', status: :ok
+
+			# if autorizada = Autorizada.where(restringida_id: params[:id], usuario_id: params[:usuario_id]).first
+
+			# 	respond_to do |format|
+			# 		if autorizada.destroy
+			# 			format.json {render json: 'Desautorizado', status: :ok }
+			# 		else
+			# 			format.json {render json: 'Error al intentar desauthorizar', status: :unprocessable_entity }
+			# 		end
+			# 	end
+			# else
+			# 	autorizada = Autorizada.new(restringida_id: params[:id], usuario_id: params[:usuario_id])
+
+			# 	respond_to do |format|
+			# 		if autorizada.save
+			# 			format.json {render json: 'Authorizado', status: :ok }
+			# 		else
+			# 			format.json {render json: 'Error al intentar autorizar', status: :unprocessable_entity }
+			# 		end
+			# 	end
+			# end
 			
 		end
 
